3e4dfb81ef522a435a70e062f6d1e15655c6117f
morpheu <153627592+Morpheusmmt@users.noreply.github.com>
2024-10-16 17:51:29 -0300

    Update game.js
    talvez ajeite questão 1

--- game.js
+++ game.js
@@ -4,7 +4,7 @@ const questionCounterText = document.getElementById("questionCounter");
 const scoreText = document.getElementById("score");
 
 let currentQuestion = {};
-let acceptingAnswers = true; // Corrigido
+let acceptingAnswers = true; 
 let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
@@ -12,26 +12,26 @@ let availableQuestions = [];
 let questions = [
     {
         question: "Quando e onde foi a primeira vez que conversamos?",
-        choice1: "26/10/2024, pedras 2", // Adicionada vírgula
-        choice2: "23/10/2024, passarela", // Adicionada vírgula
-        choice3: "10/09/2023, pinheiro", // Adicionada vírgula
-        choice4: "26/10/2023, pedras 1", // Adicionada vírgula
+        choice1: "26/10/2024, pedras 2", 
+        choice2: "23/10/2024, passarela", 
+        choice3: "10/09/2023, pinheiro", 
+        choice4: "26/10/2023, pedras 1", 
         answer: 1
     },
     {
         question: "O que aconteceu na escada do pinheiro?",
-        choice1: "Maida caiu e Emily riu", // Adicionada vírgula
-        choice2: "Maida e Emily conversaram sobre Chesterton", // Adicionada vírgula
-        choice3: "Maida disse que ela era vermelha e Emily azul e que juntas formavam roxo", // Adicionada vírgula
-        choice4: "Maida e Emily se olharam", // Adicionada vírgula
+        choice1: "Maida caiu e Emily riu", 
+        choice2: "Maida e Emily conversaram sobre Chesterton", 
+        choice3: "Maida disse que ela era vermelha e Emily azul e que juntas formavam roxo", 
+        choice4: "Maida e Emily se olharam", 
         answer: 3
     },
     {
         question: "Quais os primeiros livros que Maida deu pra Emily?",
-        choice1: "Chesterton e natureza da mordida", // Adicionada vírgula
-        choice2: "Manifesto Comunista e HQ do Miranha", // Adicionada vírgula
-        choice3: "Tudo é rio e natureza da mordida", // Adicionada vírgula
-        choice4: "Mulherzinhas e natureza da mordida", // Adicionada vírgula
+        choice1: "Chesterton e natureza da mordida", 
+        choice2: "Manifesto Comunista e HQ do Miranha", 
+        choice3: "Tudo é rio e natureza da mordida", 
+        choice4: "Mulherzinhas e natureza da mordida", 
         answer: 4
     }
 ];
@@ -48,8 +48,8 @@ startGame = () => {
     getNewQuestion();
 }
 
-getNewQuestion = () => { // Corrigido
-    if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) { // Corrigido
+getNewQuestion = () => {
+    if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
         localStorage.setItem("mostRecentScore", score);
         return window.location.assign("./end.html");
     }
@@ -66,7 +66,7 @@ getNewQuestion = () => { // Corrigido
     });
 
     availableQuestions.splice(questionIndex, 1);
-    acceptingAnswers = true; // Corrigido
+    acceptingAnswers = true; // Certifique-se de que isso está aqui
 };
 
 choices.forEach(choice => {
